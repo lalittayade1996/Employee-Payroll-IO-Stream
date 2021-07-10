21cd2b6f08b3d79a405a7c935bd2ac946ed54a7a
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-10 17:58:22 +0530

    ADDED : Welcome To Emoployee Pay Roll Data Programs

---
+++ src/main/java/com/bridgelabz/designprinciple/IO_Steram_Program/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.designprinciple.IO_Steram_Program;
+
+public class Welcome {
+public static void main(String[] args) {
+System.out.println("Welcome To Employee Pay Roll Data");
+}
+}
---
+++ src/test/java/com/bridgelabz/designprinciple/IO_Steram_Program/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.designprinciple.IO_Steram_Program;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
