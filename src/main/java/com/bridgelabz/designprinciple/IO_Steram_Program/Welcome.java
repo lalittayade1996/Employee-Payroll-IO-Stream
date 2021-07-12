1a5dc7d23fbfce329df38362299da88d5bb9b746
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-12 13:29:53 +0530

    ADDED : Read And Write Operations And Created EmployeePayRollData Class

--- src/main/java/com/bridgelabz/designprinciple/IO_Steram_Program/Welcome.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.designprinciple.IO_Steram_Program;
-
-public class Welcome {
-public static void main(String[] args) {
-System.out.println("Welcome To Employee Pay Roll Data");
-}
-}
